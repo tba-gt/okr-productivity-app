bb7b4f45a5d28fca827175b138d2d98d41d6e5c1
Sunidhi Dhawan <55981419+dsunidhi@users.noreply.github.com>
2021-08-10 20:10:47 +0530

    Create index.js

---
+++ views/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (  
+         <div>
+             <h1>An effective OKR + Timeboxing tool</h1>
+         </div>
+      );
+};
+
+export default Home;
