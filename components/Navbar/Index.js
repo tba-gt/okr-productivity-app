5494fce5b561d19b56c361cf32677deec3718738
Sunidhi Dhawan <55981419+dsunidhi@users.noreply.github.com>
2021-08-10 19:27:07 +0530

    Create Index.js

---
+++ components/Navbar/Index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+    Nav,
+    NavLogo,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+} from "./NavbarElements";
+
+const Navbar = () => {
+    return (
+        <>
+           <Nav>
+            <NavLogo to="/">
+                Macro to Micro
+                
+            </NavLogo>
+            <Bars />
+
+            <NavMenu>
+                <NavLink to="/" activeStyle>
+                    Home
+                </NavLink>
+                <NavLink to="/dashboard" activeStyle>
+                    OKR Dashboard
+                </NavLink>
+                <NavLink to="/weeklyplan" activeStyle>
+                    Weekly Plan
+                </NavLink>
+                <NavLink to="/signin" activeStyle>
+                    Sign In
+                </NavLink>
+                <NavBtn>
+                    <NavBtnLink to="/signup">Sign Up</NavBtnLink>                
+                </NavBtn>
+            </NavMenu> 
+           </Nav> 
+        </>
+    );
+};
+export default Navbar;
